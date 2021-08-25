da02c7bd01f8f168c9e0f360007b600351b8515a
Vikram Murugan <Vikramm345@gmail.com>
2021-08-25 14:54:34 -0700

    fix nozzle

--- CH1/RocketShip.java
+++ CH1/RocketShip.java
@@ -20,7 +20,7 @@ public class RocketShip {
 				System.out.print(" ");
 			}
 			System.out.print("/");
-			for (int k = 0; k <= i*2; k++) {
+			for (int k = 0; k < i*2; k++) {
 				System.out.print(" ");
 			}
 			System.out.println("\\");
