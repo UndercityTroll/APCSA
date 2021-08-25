268d5845572eae2698b96baa56c17588e2001bb8
Vikram Murugan <Vikramm345@gmail.com>
2021-08-25 13:48:23 -0700

    Initial commit

---
+++ CH1/Adress.java
@@ -0,0 +1,16 @@
+public class Adress{
+	public static void main(String[] args){
+		System.out.println("Vikram Murugan");
+		System.out.println();
+		System.out.println("123 fun st");
+		System.out.println("Fremont, CA 94568");
+		System.out.println("(510)-796-1776");
+		
+		System.out.println("\ta\tb\tc");
+		System.out.println("\\\\");
+		System.out.println("‘");
+		System.out.println("\"\"\"");
+		System.out.println("C:\nin\the downward spiral");
+		System.out.println("/ \\ // \\\\ /// \\\\\\");
+    }
+}
