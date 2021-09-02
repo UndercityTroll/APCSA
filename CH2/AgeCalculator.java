058e41e1b456ef9789d41c88636015afcea45813
Vikram Murugan <Vikramm345@gmail.com>
2021-09-02 14:54:43 -0700

    update class

--- CH2/AgeCalculator.java
+++ CH2/AgeCalculator.java
@@ -11,5 +11,13 @@ public class AgeCalculator{
 		System.out.println("I am "+(currentYear-myYearBorn)+" years old!");
 		System.out.println("My pet is "+(currentYear-myPetYearBorn)+" years old!");
 		System.out.println("My pet is "+(currentYear-myPetYearBorn)*petAgeScalar+" years old in human years!");
+
+		forLoop();
+	}
+	public static void forLoop(){
+		for(int i = 1; i<=4; i++){
+			System.out.println("2 times "+i+" = "+2*i);
+		}
+
 	}
 }
